99c117c4d13fe3ca16c452fbf3a5e197d4aabd8b
GiangLe1999 <legiangbmt09@gmail.com>
2023-11-10 16:46:35 +0700

    Fix bugs

--- app/api/public/articles/route.ts
+++ app/api/public/articles/route.ts
@@ -1,12 +1,16 @@
 import dbConnect from "@/lib/db";
 import Article from "@/models/Article";
 import { NextResponse } from "next/server";
+export const dynamic = "force-dynamic";
 
 export async function GET(req: Request) {
   try {
+    const { searchParams } = new URL(req.url);
+    const specifiedProps = searchParams.get("specifiedProps");
+
     await dbConnect();
 
-    const articles = await Article.find();
+    const articles = await Article.find().select(specifiedProps || "");
 
     return NextResponse.json({ ok: true, articles });
   } catch (error: any) {
--- app/api/public/categories/route.ts
+++ app/api/public/categories/route.ts
@@ -1,12 +1,16 @@
 import dbConnect from "@/lib/db";
 import Category from "@/models/Category";
 import { NextResponse } from "next/server";
+export const dynamic = "force-dynamic";
 
 export async function GET(req: Request) {
   try {
+    const { searchParams } = new URL(req.url);
+    const specifiedProps = searchParams.get("specifiedProps");
+
     await dbConnect();
 
-    const categories = await Category.find();
+    const categories = await Category.find().select(specifiedProps || "");
 
     return NextResponse.json({ ok: true, categories });
   } catch (error: any) {
