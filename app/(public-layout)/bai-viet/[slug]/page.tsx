e1edfbd43c3e3299c5cc1524e29fe296d7770c9b
GiangLe1999 <legiangbmt09@gmail.com>
2023-11-22 16:06:26 +0700

    Fix UI for article page

--- app/(public-layout)/bai-viet/[slug]/page.tsx
+++ app/(public-layout)/bai-viet/[slug]/page.tsx
@@ -29,86 +29,90 @@ const Page: NextPage<Props> = async ({ params }) => {
           />
         </div>
       </div>
-
-      <div className="pl-8 mt-28 w-[calc(100vw_-_(48%_+_40px)_-_(100vw_-_100%)_/_2)]">
-        <div className="flex items-center gap-2">
-          <BtnWithIcon
-            icon={FaAngleLeft}
-            content=""
-            customClasses="grid place-items-center h-[30px] w-[30px] !p-0 !rounded-full before:!rounded-full !text-sm"
-            to="/"
-          />
-
-          {article?.country?.name && (
+      <div className="container">
+        <div className="mt-28 w-1/2">
+          <div className="flex items-center gap-2">
             <BtnWithIcon
-              to=""
-              content={article.country.name}
-              customClasses="!py-1 !rounded-[40px] before:!rounded-[40px] !text-sm"
+              icon={FaAngleLeft}
+              content=""
+              customClasses="grid place-items-center h-[30px] w-[30px] !p-0 !rounded-full before:!rounded-full !text-sm"
+              to="/"
             />
-          )}
 
-          {article?.region?.name && (
-            <BtnWithIcon
-              to=""
-              content={article.region.name}
-              customClasses="!py-1 !rounded-[40px] before:!rounded-[40px] !text-sm"
-            />
-          )}
-        </div>
+            {article?.country?.name && (
+              <BtnWithIcon
+                to=""
+                content={article.country.name}
+                customClasses="!py-1 !rounded-[40px] before:!rounded-[40px] !text-sm"
+              />
+            )}
 
-        <h1 className="text-black_text font-black text-3xl my-8 leading-10 tracking-wide font-arima">
-          {article?.name}
-        </h1>
+            {article?.region?.name && (
+              <BtnWithIcon
+                to=""
+                content={article.region.name}
+                customClasses="!py-1 !rounded-[40px] before:!rounded-[40px] !text-sm"
+              />
+            )}
+          </div>
 
-        <p className="text-sm flex items-center gap-1 mb-2">
-          <FcAlarmClock />{" "}
-          <span>
-            Đã đăng: <strong>{formatLongDate(article?.createdAt || "")}</strong>
-          </span>
-        </p>
+          <h1 className="text-black_text font-black text-3xl my-8 leading-10 tracking-wide font-arima">
+            {article?.name}
+          </h1>
 
-        <div className="flex items-center gap-4 text-sm flex-wrap">
-          <span>
-            Bởi{" "}
-            <Link
-              href=""
-              className="underline font-extrabold text-admin_primary"
-            >
-              {article?.author?.name}
-            </Link>
-          </span>
-
-          {article?.category?.name && (
+          <p className="text-sm flex items-center gap-1 mb-2">
+            <FcAlarmClock />{" "}
             <span>
-              Danh mục:{" "}
+              Đã đăng:{" "}
+              <strong>{formatLongDate(article?.createdAt || "")}</strong>
+            </span>
+          </p>
+
+          <div className="flex items-center gap-4 text-sm flex-wrap">
+            <span>
+              Bởi{" "}
               <Link
                 href=""
                 className="underline font-extrabold text-admin_primary"
               >
-                {article.category.name}
+                {article?.author?.name}
               </Link>
             </span>
-          )}
 
-          {article?.interest?.name && (
-            <span>
-              Sở thích:{" "}
-              <Link
-                href=""
-                className="underline font-extrabold text-admin_primary"
-              >
-                {article.interest.name}
-              </Link>
-            </span>
-          )}
+            {article?.category?.name && (
+              <span>
+                Danh mục:{" "}
+                <Link
+                  href=""
+                  className="underline font-extrabold text-admin_primary"
+                >
+                  {article.category.name}
+                </Link>
+              </span>
+            )}
+
+            {article?.interest?.name && (
+              <span>
+                Sở thích:{" "}
+                <Link
+                  href=""
+                  className="underline font-extrabold text-admin_primary"
+                >
+                  {article.interest.name}
+                </Link>
+              </span>
+            )}
+          </div>
+
+          <hr className="mt-6 mb-4" />
+
+          <p className="italic leading-8 text-justify">
+            {article?.description}
+          </p>
         </div>
-
-        <hr className="mt-6 mb-4" />
-
-        <p className="italic leading-8">{article?.description}</p>
       </div>
 
-      <div className="container grid grid-cols-1 lg:grid-cols-[0.9fr,0.4fr] gap-10">
+      <div className="container grid grid-cols-1 lg:grid-cols-[0.9fr,0.4fr] gap-10 mt-6">
         <div className="content prose prose-img:w-full prose-h2:text-admin_primary prose-h2:font-extrabold prose-h3:font-extrabold text-justify">
           <ArticleContent article={article} />
 
